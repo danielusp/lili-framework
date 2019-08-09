33f9ecf2ecf187563f971a51680fe9f20dc3ef96
Daniel Mendonça <dmsantos@uolinc.com>
2019-08-09 16:54:46 -0300

    BUGFIX: Database error alert

--- services/Base.class.php
+++ services/Base.class.php
@@ -8,6 +8,12 @@
 		public function __construct()  
 		{
 			$this->dbConnect = mysqli_connect( DB_HOST , DB_USER , DB_PASS , DB_NAME );
+
+			/* check connection */
+			if (mysqli_connect_errno() && ERROR_REPORTING === 1) {
+				printf("Connect failed: %s\n", mysqli_connect_error());
+				exit();
+			}
 		}
 
 	}
