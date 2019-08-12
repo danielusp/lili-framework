f1fe3c698b0bb9d83752d65527b1ccd4a160c04e
Daniel Mendonça <dmsantos@uolinc.com>
2019-08-12 12:54:19 -0300

    NEW: Receives a pure json POST

--- services/Form.class.php
+++ services/Form.class.php
@@ -2,11 +2,20 @@
 	
 	Class Form
 	{
+		// Receives a regular POST
 		public function getData()
 		{
 			$data = file_get_contents("php://input");
 			parse_str($data, $array);
 			
 			return $array;
+		}
+
+		// Receives a json POST in the body
+		public function getJsonData()
+		{
+			$data = file_get_contents("php://input");
+			
+			return json_decode($data);
 		}
 	}
